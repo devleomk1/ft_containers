8e291cfd0fe47dc6b6c622bdb1c12df4845ab0a1
Kang Jisoon <wltnskjs@naver.com>
2022-09-26 18:00:17 +0900

    🌱 Add Iterator_traits.hpp

---
+++ iterator_traits.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator_traits.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 13:47:23 by jisokang          #+#    #+#             */
+/*   Updated: 2022/09/26 17:58:38 by jisokang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITERATOR_TRAITS_HPP
+# define ITERATOR_TRAITS_HPP
+
+namespace ft
+{
+	//24.3 primitives:
+	template<class Iterator> struct iterator_traits
+	{
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::value_type			value_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference			reference;
+		typedef typename Iterator::iterator_category	iterator_category;
+	};
+
+	template<class T> struct iterator_traits<T*>
+	{
+		typedef ptrdiff_t					difference_type;
+		typedef T							value_type;
+		typedef T*							pointer;
+		typedef T&							reference;
+		typedef random_access_iterator_tag	iterator_category;
+	};
+
+	template<class T> struct iterator_traits<const T*>
+	{
+		typedef ptrdiff_t					difference_type;
+		typedef T							value_type;
+		typedef const T						pointer;
+		typedef const T&					reference;
+		typedef random_access_iterator_tag	iterator_category;
+	};
+
+}
+
+#endif
