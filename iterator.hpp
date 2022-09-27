33a2e4ae6adfcba9e0928378c782023c7854d3ca
Kang Jisoon <wltnskjs@naver.com>
2022-09-27 21:26:13 +0900

    🏷️ Add basic iterator definition

--- iterator.hpp
+++ iterator.hpp
@@ -6,23 +6,31 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 20:35:21 by jisokang          #+#    #+#             */
-/*   Updated: 2022/09/25 23:01:46 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/09/27 21:24:03 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITERATOR_HPP
 # define ITERATOR_HPP
 
-
+# include <cstddef>				//FOR ptrdiff_t (https://en.cppreference.com/w/cpp/types/ptrdiff_t)
+# include "iterator_traits.hpp"
 
 namespace ft
 {
 	//24.3 primitives:
-	template<class Iterator> struct iterator_traits;
-	template<class T> struct iterator_traits<T*>;
+	//template<class Iterator> struct iterator_traits;
+	//template<class T> struct iterator_traits<T*>;
 
-	template<class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&> struct iterator;
-														//ptrdiff_t는 또 뭐시당가
+														//ptrdiff_t는 또 뭐시당가 -> 찾아보자
+	template<class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&> struct iterator
+	{
+		typedef T			value_type;
+		typedef Distance	difference_type;
+		typedef Pointer		pointer;
+		typedef Reference	reference;
+		typedef Category	iterator_category;
+	};
 
 	//tag는 머시당가?
 	struct input_iterator_tag {};
@@ -92,17 +100,17 @@ namespace ft
 				const reverse_iterator<Iterator>& y
 			);
 
-	template <class Container> class back_insert_iterator;
-	template <class Container>
-		back_insert_iterator<Container> back_insert(Container& x);
+	//template <class Container> class back_insert_iterator;
+	//template <class Container>
+	//	back_insert_iterator<Container> back_insert(Container& x);
 
-	template <class Container> class front_insert_iterator;
-	template <class Container>
-		front_insert_iterator<Container> front_insert(Container& x);
+	//template <class Container> class front_insert_iterator;
+	//template <class Container>
+	//	front_insert_iterator<Container> front_insert(Container& x);
 
-	template <class Container> class insert_iterator;
-	template <class Container, class Iterator>
-		insert_iterator<Container> inserter(Container& x, Iterator i);
+	//template <class Container> class insert_iterator;
+	//template <class Container, class Iterator>
+	//	insert_iterator<Container> inserter(Container& x, Iterator i);
 
 	//24.5 stream iterators:
 	//template <class T, class charT = char, class traits = char_traits<charT>, class Distance = ptrdiff_t>
---
+++ random_access_iterator.hpp
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   random_access_iterator.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 20:59:10 by jisokang          #+#    #+#             */
+/*   Updated: 2022/09/27 21:13:35 by jisokang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RANDOM_ACCESS_ITERATOR_HPP
+# define RANDOM_ACCESS_ITERATOR_HPP
+
+#endif
