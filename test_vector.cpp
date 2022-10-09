2a0590f8453ecdc0d96065d994d2add3ddd0a276
Kang Jisoon <wltnskjs@naver.com>
2022-10-09 17:37:03 +0900

    🧪 Add test_vector.hpp

---
+++ test_vector.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_vector.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/09 17:36:35 by jisokang          #+#    #+#             */
+/*   Updated: 2022/10/09 17:36:36 by jisokang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include "color.hpp"
+
+void print_vector(std::vector<int> vec)
+{
+	std::vector<int>::iterator	it = vec.begin();
+	int	i = 0;
+	std::cout << "vec.size()     : " << vec.size() << "\n";
+	std::cout << "vec.capacity() : " << vec.capacity() << "\n";
+	while (it != vec.end())
+	{
+		std::cout << i << "- [" << *it << "]" << std::endl;
+		i++;
+		it++;
+	}
+
+}
+
+int main()
+{
+
+	std::vector<int>	vec;
+
+	for (size_t i = 1; i <= 100; i++)
+		vec.push_back(i);
+	std::cout << "[1] [2] ... [100] [0] ... \n";
+	std::cout << " vec." YELLOW "size" RESET "()\t\t: " << vec.size() << "\n";
+	std::cout << " vec." YELLOW "capacity" RESET "()\t\t: " << vec.capacity() << "\n";
+	std::cout << "*vec." YELLOW "begin" RESET "()\t\t: " << *vec.begin() << "\n";
+	std::cout << "*vec." YELLOW "end" RESET "()\t\t: " << *vec.end() << "\n";
+	std::cout << "&vec." YELLOW "begin" RESET "()\t\t: " << &(*vec.begin()) << "\n";
+	std::cout << "&vec." YELLOW "rend" RESET "()\t\t: " << &(*vec.rend()) << "\n";
+	std::cout << "&vec." YELLOW "end" RESET "()\t\t: " << &(*vec.end()) << "\n";
+	std::cout << " vec." YELLOW "front" RESET "()\t\t: " << vec.front() << "\n";
+	std::cout << " vec." YELLOW "back" RESET "()\t\t: " << vec.back() << "\n";
+	std::cout << " vec." YELLOW "at" RESET "(3)\t\t: " << vec.at(3) << "\n";
+	std::cout << " vec[3]\t\t\t: " << vec[3] << "\n";
+	vec.reserve(200);
+	std::cout << " vec." YELLOW "reserve" RESET "(200)\t: capacity() = " << vec.capacity() << "\n";
+
+	return (0);
+}
