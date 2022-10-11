3f527ad6939a170a31b00565377f6148a93d6c43
Kang Jisoon <wltnskjs@naver.com>
2022-10-11 21:30:21 +0900

    🏷️ Add enable_if declaration

--- util.hpp
+++ util.hpp
@@ -6,11 +6,18 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 16:40:59 by jisokang          #+#    #+#             */
-/*   Updated: 2022/10/10 19:45:57 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/10/11 21:28:45 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTIL_HPP
 # define UTIL_HPP
+
+namespace ft
+{
+	//enable_if	(https://cplusplus.com/reference/type_traits/enable_if/?kw=enable_if)
+	template<bool Cond, class T = void> struct enable_if {};
+	template<class T> struct enable_if<true, T> { typedef T type; };
+}
 
 #endif
