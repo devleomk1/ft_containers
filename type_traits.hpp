71825c3dee91e43b8174e3cecf076f838b621361
Kang Jisoon <wltnskjs@naver.com>
2022-10-14 16:23:53 +0900

    🚚 Rename util.hpp → type_traits.hpp

--- type_traits.hpp
+++ type_traits.hpp
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   util.hpp                                           :+:      :+:    :+:   */
+/*   type_traits.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 16:40:59 by jisokang          #+#    #+#             */
-/*   Updated: 2022/10/11 21:28:45 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/10/14 16:23:06 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTIL_HPP
-# define UTIL_HPP
+#ifndef TYPE_TRAITS_HPP
+# define TYPE_TRAITS_HPP
 
 namespace ft
 {
 	//enable_if	(https://cplusplus.com/reference/type_traits/enable_if/?kw=enable_if)
 	template<bool Cond, class T = void> struct enable_if {};
 	template<class T> struct enable_if<true, T> { typedef T type; };
+
 }
 
 #endif
